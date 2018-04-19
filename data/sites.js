2f391b2ed8d285a0f27b2cae6dae987dd1bbb4e0
David Legrand <david@nextinpact.com>
2018-04-19 21:53:54 +0200

    Version initiale

---
+++ data/sites.js
@@ -0,0 +1,15 @@
+/* Définition des statuts :
+---------------------------
+
+1 : Stocke les mots de passe en clair, en cours de correction
+2 : Stocke les mots de passe en clair, pas de correction ou de réponse
+
+*/
+
+const sites = {
+    "list":
+    [
+        { "id": "1", "name":"Les Echos", "url": "https://www.lesechos.fr", "entry_date": "1524159327719", "status":"1" },
+        { "id": "2", "name":"Societe.com", "url": "https://www.societe.com", "entry_date": "1524159327720", "status":"2" }
+    ]
+}
---
+++ js/getData.js
@@ -0,0 +1,36 @@
+function showData (sites) {
+    let sitesList = sites.list
+    sitesList.sort((a, b) => parseFloat(b.entry_date) - parseFloat(a.entry_date));
+
+    sitesList.forEach(element => {
+        document.getElementById("data").appendChild(dataHtmlFormat(element));
+    });
+}
+
+function dataHtmlFormat(data){
+    let date = new Date(parseInt(data.entry_date));
+    let text = " a  été ajouté dans notre base le " + date.toLocaleDateString("fr-FR") + ". ";
+
+    switch(data.status)
+    {
+        case "1":
+            text += "Une correction est en cours.";
+            break;
+
+        case "2":
+            text += "Aucune correction ne semble en cours."  
+        break;
+    }
+
+    let lien = document.createElement("a");
+    lien.href = data.url;
+    lien.innerText = data.name;
+    lien.target = "_blank";
+
+    let li = document.createElement("li");
+    let content = document.createTextNode(text);
+    li.appendChild(lien);
+    li.appendChild(content);
+
+    return li;
+}
